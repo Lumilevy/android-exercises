b7f28caf8690bc2927d811932f028c0fed784e78
Lumilevy <flora_pelletier@yahoo.com>
2017-11-09 16:51:20 +0100

    Exercice liste

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,8 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,9 @@ public class LibraryActivity extends AppCompatActivity {
 
         List<Book> books = getBooks();
 
-        // TODO findViewById() and setAdapter()
+        ListView list = findViewById(R.id.bookListView);
+        list.setAdapter(new BookAdapter(this,books));
+
     }
 
     private List<Book> getBooks() {
